f1ffb321ad608d8fc4c70555436a1ab047570669
danylosant <do@digiants.com.ua>
2025-01-17 20:22:37 +0200

    created Promocodes model

---
+++ app/Models/Promocode.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use App\FastAdminPanel\Models\MultilanguageModel;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Promocode extends Model
+{
+    use HasFactory;
+
+    protected $table = 'promocodes';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+    ];
+
+    #region Relationships
+    
+    
+
+    #endregion
+}
